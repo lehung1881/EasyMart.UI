3e1fadc42da76f2eee7671af9140c6f43b254493
le hung <hungle180801@gmail.com>
2026-06-14 01:08:32 +0700

    Update uuid v4

--- src/commons/commonFunction.ts
+++ src/commons/commonFunction.ts
@@ -1,5 +1,5 @@
 type DataType = "string" | "boolean" | "number";
-import { uuidv7 } from "uuidv7";
+import { uuidv7, uuidv4 } from "uuidv7";
 
 class CommonFunction {
     formatNumber(number: number, decimalPlaces: number = 0): string | null {
@@ -19,11 +19,7 @@ class CommonFunction {
      * @returns
      */
     generateGUIDV4(): string {
-        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-            const r = (Math.random() * 16) | 0;
-            const v = c === "x" ? r : (r & 0x3) | 0x8;
-            return v.toString(16);
-        });
+        return uuidv4();
     }
 
     /**
